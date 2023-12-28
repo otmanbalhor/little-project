0fad464bb95f5303ba5902e1930a8ac3a601a3d4
otman <otman@DESKTOP-4RA473J.localdomain>
2023-12-28 14:44:31 +0100

    début

---
+++ script.js
@@ -0,0 +1,70 @@
+
+
+const pays=[
+    {
+        flag:"",
+        name:"Belgium",
+        capital:"Brussels",
+        population:"11,59 M",
+        bigcity:"Antwerp, Charleroi, Liège and Ghent",
+        executive:"Alexander De Croo",
+        dishes:"French fries and mussels"
+
+    },
+
+    {
+        flag:"",
+        name:"France",
+        capital:"Paris",
+        population:"67,75 M",
+        bigcity:"Marseille, Lyon, Toulouse and Nice",
+        executive:"Emmanuel Macron",
+        dishes:"Provencal ratatouille"
+
+    },
+
+    {
+        flag:"",
+        name:"Spain",
+        capital:"Madrid",
+        population:"47,42 M",
+        bigcity:"Barcelona, Valencia, Sevilla and Zaragoza",
+        executive:"Pedro Sanchez",
+        dishes:"Paella"
+    },
+
+    {
+        flag:"",
+        name:"Italy",
+        capital:"Roma",
+        population:"59,11 M",
+        bigcity:"Milano, Napoli, Torino, Palermo",
+        executive:"Sergio Mattarella",
+        dishes:"Pizza"
+
+    },
+
+    {
+        flag:"",
+        name:"U.S.A.",
+        capital:"Washington",
+        population:"331,9 M",
+        bigcity:"New York City, Los Angeles, Chicago, Houston",
+        executive:"Joe Biden",
+        dishes:"Mac and cheese"
+
+    },
+
+    {
+        flag:"",
+        name:"Germany",
+        capital:"Berlin",
+        population:"83,2 M",
+        bigcity:"Hamburg, Munich, Köln, Frankfurt am Main",
+        executive:"Frank-Walter Steinmeier",
+        dishes:"Sauerkraut"
+
+    },
+
+    
+]
